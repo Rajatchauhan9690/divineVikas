228e649b137fb3f966732a826eeb81219c4e1cdd
Rajat chauhan <rajatchauhan9690@gmail.com>
2026-03-05 19:56:44 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -38,25 +38,9 @@ MIDDLEWARE
 ========================================
 */
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   }),
-// );
-const allowedOrigins = process.env.FRONTEND_URL.split(",").map((origin) =>
-  origin.trim(),
-);
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
